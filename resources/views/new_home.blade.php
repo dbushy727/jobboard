583e46c86e0fc8bab2b0cec8f1a61647653f6e57
Sean S. King <seanscottking@gmail.com>
2016-07-08 17:12:16 -0400

    Add meta description

--- resources/views/new_home.blade.php
+++ resources/views/new_home.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="A curated list of {{ env('JOB_TYPE') }} opportunities">
     <title>{{ env('APP_NAME') }}</title>
     <!-- Fonts -->
     <link href='https://fonts.googleapis.com/css?family=Lora:400,700' rel='stylesheet' type='text/css'>
