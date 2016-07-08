3ad7756252bd75ee31115a13a6521e099d59508d
Sean S. King <seanscottking@gmail.com>
2016-07-08 16:25:12 -0400

    Add contact info to footer

--- resources/views/new_home.blade.php
+++ resources/views/new_home.blade.php
@@ -74,7 +74,10 @@
     <!-- Footer -->
     <footer class="footer">
         <div class="container-fluid">
-            <a href="http://voyent.io" class="navbar-text pull-right">Powered by Voyent</a>
+            <p class="navbar-text pull-right">
+                <a href="mailto:{{ env('ADMIN_EMAIL') }}">{{ env('ADMIN_EMAIL') }}</a>
+                <a href="{{ env('TWITTER_URL') }}">{{ env('TWITTER_HANDLE') }}</a>
+            </p>
             <span class="pull-left navbar-text">&copy; {{ date('Y') }} | <a href="/jobs/feed">RSS Feed</a></span>
         </div>
     </footer>
@@ -82,5 +85,16 @@
 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     <script src="/js/all.js"></script>
+
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    
+      ga('create', 'UA-80504881-1', 'auto');
+      ga('send', 'pageview');
+    
+    </script>
 </body>
 </html>
