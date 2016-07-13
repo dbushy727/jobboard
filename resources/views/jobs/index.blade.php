8c2d5e51c5faeddcf3cf403330c2a7aeed2305a7
dbushy727 <d.bushkanets@gmail.com>
2016-07-13 16:34:17 -0400

    fixed search button

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -4,25 +4,23 @@
         <p>
             Welcome to <a href="{{ url('/') }}"><b>{{ env('APP_NAME') }}</b></a>. We serve the {{ env('JOB_TYPE') }} community by offering the best place to find and list {{ env('JOB_TYPE') }} career opportunities.
         </p>
-    </div>
-    <div class="col-sm-4 pull-right">
-        @include('post-job')
-    </div>
-    <div class="col-sm-8 clearfix">
         <form class="search" action="/jobs/search">
-            <input type="text" name="term" class="form-control" maxlength="64" placeholder="Search for jobs"/>
-            <button type="submit" class="btn btn-red search-button">Search</button>
+            <div class="input-group">
+                <input type="text" name="term" class="form-control" maxlength="64" placeholder="Search for jobs">
+                <span class="input-group-btn"><button class="btn btn-red search-button" type="submit">Search</button></span>
+            </div>
         </form>
         @if(isset($term))
             <div>Search: {{ $term }} <a href="/"><i class="fa fa-times"></i></a></div>
         @endif
-    </div>
-    {{-- Two Column Layout --}}
-    <div class="col-sm-8">
+
         <div class="jobs">
             <div class="col-sm-12 no-pad">
                 @include('jobs.partials.list')
             </div>
         </div>
+    </div>
+    <div class="col-sm-4">
+        @include('post-job')
     </div>
 @endsection
