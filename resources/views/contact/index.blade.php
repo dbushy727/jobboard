fcacc26d07f64906727bb9f12d31b314f3564edb
Sean S. King <seanscottking@gmail.com>
2016-10-25 21:49:02 -0400

    Remove placeholders and center div

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,8 +1,8 @@
 @extends('new_home')
 @section('content')
-    <h1 class="text-center">Contact Form</h1>
+    <h1 class="text-center">Contact Us</h1>
 
-    <div class="panel panel-default">
+    <div class="panel panel-default col-sm-12 col-md-8 col-md-offset-2">
         <form
             action="/contact/submit"
             method="POST"
@@ -15,21 +15,21 @@
             @foreach($errors->get('name') as $error)
               <div class="text-danger">{{ $error }}</div>
             @endforeach
-            <input type="text" class="form-control" id="name" placeholder="Johnny Doe" name="name">
+            <input type="text" class="form-control" id="name" name="name">
           </div>
           <div class="form-group">
-            <label for="email">Email address*</label>
+            <label for="email">Return email address*</label>
             @foreach($errors->get('email') as $error)
               <div class="text-danger">{{ $error }}</div>
             @endforeach
-            <input type="text" class="form-control" id="email" placeholder="email@example.com" name="email">
+            <input type="text" class="form-control" id="email" name="email">
           </div>
           <div class="form-group">
             <label for="message">Message*</label>
             @foreach($errors->get('body') as $error)
               <div class="text-danger">{{ $error }}</div>
             @endforeach
-            <textarea class="form-control" rows="3" placeholder="Site looks awesome!! I would love to learn more about {{env('APP_NAME')}}." name="body"></textarea>
+            <textarea class="form-control" rows="3" name="body"></textarea>
           </div>
           {{ csrf_field() }}
           <button type="submit" class="btn btn-success">Submit</button>
