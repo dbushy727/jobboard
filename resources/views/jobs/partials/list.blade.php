c687ee0978e385c996fc0463e9ec6dd1b1c8fe50
dbushy727 <d.bushkanets@gmail.com>
2017-02-01 20:39:34 -0500

    added microdata to search result page

--- resources/views/jobs/partials/list.blade.php
+++ resources/views/jobs/partials/list.blade.php
@@ -3,10 +3,29 @@
 @else
     <div class="list-group job-list tight">
     @foreach($jobs as $job)
-        <a href="/posts/{{$job->slug}}" class="list-group-item {{ $job->is_featured ? 'featured' : ''}}" >
+        <a
+            href="/posts/{{$job->slug}}"
+            class="list-group-item {{ $job->is_featured ? 'featured' : ''}}"
+            itemscope
+            itemtype="http://schema.org/JobPosting">
             <div class="pull-right job-list-date">
-                <div class="job-list-item location">{{ $job->location }}</div>
-                <!-- <div class="job-list-item date">{{ $job->date->format('M d Y') }}</div> -->
+                <div class="job-list-item location"
+                    itemprop="jobLocation"
+                    itemscope
+                    itemtype="http://schema.org/Place">
+
+                    <span
+                        itemprop="address"
+                        itemscope
+                        itemtype="http://schema.org/PostalAddress">
+                        <span itemprop="addressLocality">{{ $job->location }}</span>
+                    </span>
+                </div>
+                <div
+                    class="job-list-item date"
+                    style="display: none;">
+                    <span itemprop="datePosted">{{ $job->date->toDateString() }}</span>
+                </div>
                 <div class="job-list-item date">{{ $job->date->diffForHumans() }}</div>
             </div>
             <div>
@@ -16,8 +35,14 @@
                     <img class="job-posting-logo" src="/img/building.png" alt="Default Logo">
                 @endif
                 <div class="job-list-info">
-                    <div class="job-list-title">{{$job->title}}</div>
-                    <div class="job-list-company">{{$job->company_name}}</div>
+                    <div class="job-list-title" itemprop="title">{{$job->title}}</div>
+                    <div
+                        class="job-list-company"
+                        itemprop="hiringOrganization"
+                        itemscope
+                        itemtype="http://schema.org/Organization">
+                        <span itemprop="name">{{$job->company_name}}</span>
+                    </div>
                     @if($job->date->diffInDays() < 2)
                         <div class="label label-info small">New</div>
                         @if($job->isReplacement())
